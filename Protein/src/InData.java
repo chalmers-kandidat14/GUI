a00c027a1d8514e35d76ad493c1b8a83b3403874
Andreas Nilsson <per.andreas.nilsson@gmail.com>
2014-02-28 14:52:54 +0100

    Changed InData.java

--- Protein/src/InData.java
+++ Protein/src/InData.java
@@ -3,19 +3,19 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
  
 public class InData {
     public static void main(String[] args) {
         String fileName = "Exempel.txt";
-		Scanner r;
  
         try {
-            r = new Scanner(fileName);
-			while(r.hasNext()) {
-				System.out.println(r.nextLine());
-			}
+            List<String> lines = Files.readAllLines(Paths.get(fileName),
+                    Charset.defaultCharset());
+            for (String line : lines) {
+                System.out.println(line);
+            }
         } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
