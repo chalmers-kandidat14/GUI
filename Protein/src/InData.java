828ac0585f1190638773a3d216fdcd21e08933a0
Andreas Nilsson <per.andreas.nilsson@gmail.com>
2014-02-28 14:17:45 +0100

    Read from file

---
+++ Protein/src/InData.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+ 
+public class InData {
+    public static void main(String[] args) throws IOException {
+        String fileName = "Exempel.txt";
+ 
+        List<String> lines = Files.readAllLines(Paths.get(fileName),
+				Charset.defaultCharset());
+		String[] points = lines.toArray(new String[lines.size()]);
+		
+		System.out.println(points);
+	}
+}
