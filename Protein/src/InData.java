4355ae3dc98791eb026695b6f4192964dd241b01
Andreas Nilsson <per.andreas.nilsson@gmail.com>
2014-02-28 14:30:24 +0100

    Exempelfil

--- Protein/src/InData.java
+++ Protein/src/InData.java
@@ -3,7 +3,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
- 
+
 public class InData {
     public static void main(String[] args) throws IOException {
         String fileName = "Exempel.txt";
