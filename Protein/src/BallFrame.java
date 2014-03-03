984c20f903b739108bd6f18d21ac8619a118a8e3
Johnny Ngu <johnnyngu@gmail.com>
2014-03-03 10:19:22 +0100

    mergat ballframe

--- Protein/src/BallFrame.java
+++ Protein/src/BallFrame.java
@@ -190,14 +190,15 @@ public class BallFrame extends GLCanvas implements GLEventListener, KeyListener 
 		float currZ = CreateBall.getZ(i);
 		String colorB = CreateBall.getC(i);
 		gl.glTranslatef(currX, currY, currZ);
-		if(colorB.equals("H")){
+		
+		if(colorB.equals("P")){
 			float[] rgba = { 0.8f, 0.1f, 0.0f };
 			gl.glMaterialfv(GL.GL_FRONT, GL2.GL_AMBIENT, rgba, 0);
 			gl.glMaterialfv(GL.GL_FRONT, GL2.GL_SPECULAR, rgba, 0);
 			gl.glMaterialf(GL.GL_FRONT, GL2.GL_SHININESS, 0.5f);
 			
 		}
-		else if(colorB.equals("P")){
+		else if(colorB.equals("H")){
 			float[] rgba = { 0.0f, 0.2f, 1.0f };
 			gl.glMaterialfv(GL.GL_FRONT, GL2.GL_AMBIENT, rgba, 0);
 			gl.glMaterialfv(GL.GL_FRONT, GL2.GL_SPECULAR, rgba, 0);
